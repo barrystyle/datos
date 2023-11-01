615a9f62461967ef9d82f154b42802a1050df368
barrystyle <barrystyle@westnet.com.au>
2023-11-01 10:50:43 +0800

    bugfix: prevent weird headers from entering blockchain early phase

--- src/miner.cpp
+++ src/miner.cpp
@@ -225,7 +225,7 @@ std::unique_ptr<CBlockTemplate> BlockAssembler::CreateNewBlock(const CScript& sc
         UpdateTime(pblock, chainparams.GetConsensus(), pindexPrev);
     }
     pblock->nBits          = GetNextWorkRequired(pindexPrev, pblock, chainparams.GetConsensus());
-    pblock->nNonce         = 0;
+    pblock->nNonce         = !fProofOfStake;
     pblocktemplate->nPrevBits = pindexPrev->nBits;
     pblocktemplate->vTxSigOps[0] = GetLegacySigOpCount(*pblock->vtx[0]);
 
